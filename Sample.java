a8211bda9493d145da31d094f2b257a117f09877
sahuanshu1906 <sahuanshu1913@gmail.com>
2025-01-16 20:02:10 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,9 @@
+class Sample
+  {
+    public static void main (String[] args)
+    {
+      System.out.println("Hello  World!"); 
+      System.out.println("Welcome to DevOps");
+      System.out.println("enabled webhook");
+    }
+  }
